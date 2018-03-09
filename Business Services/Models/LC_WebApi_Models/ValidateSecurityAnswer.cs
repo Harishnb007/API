75ee83ff249bad4eea4b9dc4fa61f993a9f54e3b
Avinash <avinash.acharya@tcs.com>
2018-03-09 10:48:35 -0500

    LMA-1124 Password is not being generated when user does forgot password function on Mobile Device.

---
+++ Business Services/Models/LC_WebApi_Models/ValidateSecurityAnswer.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Services.Models.LC_WebApi_Models
+{
+    public class ValidateSecurityAnswer
+    {
+        [JsonProperty]
+        public string msg { get; set; }
+
+        [JsonProperty]
+        public Client client { get; set; }
+
+        [JsonProperty]
+        public User user { get; set; }
+
+        [JsonProperty]
+        public Userloan userLoan { get; set; }
+    }
+   
+}
